91bbe57ad084e3a5fcbcdb62511ba20950e19f2c
googlyy <googlybeast1@gmail.com>
2019-09-17 11:57:35 +0100

    Pet now gets hungry and starves on its birthday

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,9 @@
 const Pet = require('../src/pet');
 
-describe('constructor', () => {
-    it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
-    });
+describe('growUp', () => {
+  it('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.age).toEqual(1);
   });
+});
