34ca35d7299594bd118d26b7b2a10b5571b039b7
googlyy <googlybeast1@gmail.com>
2019-09-17 12:22:35 +0100

    Pet now dies at the age of 2 if not fed

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,9 +1,10 @@
 const Pet = require('../src/pet');
 
-describe('growUp', () => {
-  it('increments the age by 1', () => {
-    const pet = new Pet('Fido');
-    pet.growUp();
-    expect(pet.age).toEqual(1);
+describe('walk', () => {
+  it('increases fitness by to a maximum of 10', () => {
+    const pet = new Pet('fido');
+    pet.fitness = 8;
+    pet.walk();
+    expect(pet.fitness).toEqual(10);
   });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,8 +1,11 @@
+const MAXIMUM_FITNESS = 10;
+const MINIMUM_HUNGER = 0;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
     this.hunger = 0;
-    this.fitness = 10;
+    this.fitness = MAXIMUM_FITNESS;
   }
   
   Pet.prototype.growUp = function() {
@@ -10,5 +13,21 @@ function Pet(name) {
     this.hunger += 5;
     this.fitness -= 3;
   };
+
+  Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS ) {
+      this.fitness += 4;
+    } else {
+      this.fitness = MAXIMUM_FITNESS;
+    }
+  }
+
+  Pet.prototype.feed = function() {
+    if ((this.hunger - 3) <= MINIMUM_HUNGER ) {
+      this.hunger -= 3;
+    }  else {
+        this.hunger = MINIMUM_HUNGER;
+    }
+  }
 
 module.exports = Pet;
